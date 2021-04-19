4618b77529f4643b8fe830d0576dbf2e1e518f52
muokapwambua <muokapwambua@gmail.com>
2021-04-19 14:54:09 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,69 @@
+import os
+import io
+from PIL import Image
+from werkzeug.utils import secure_filename
+from flask import request
+
+# path to image upload
+upload_path = os.path.abspath('uploads')
+
+
+def image_extenstion(filename):
+# to check if image extenstion type
+    exts = {'jpg', 'jpeg', 'png', 'ico', 'gif'}
+    ext = filename.rsplit('.', 1)[1].lower()
+
+    if ext in exts:
+        return True
+    return False    
+
+def optimizer(image, size, extend_name):
+    """ this function takes image file for resizing
+    according to the given size and a name to extend to the original filename"""
+    try:
+        
+        filename = secure_filename(image.filename)
+        ext = filename.split('.')[-1]
+        name = filename.split('.')[0]
+
+        filepath = f'{os.path.join(path, name)}-{extend_name}.{ext}'
+        
+        #opening image for optimizing
+
+        img = Image.open(image)
+        img.thumbnail(size, Image.LANCZOS)
+        img.save(filepath, optimize=True, quality=95)
+
+        #  url to save to the database
+        urlpath = str(request.url_root)[:-1] + filepath
+
+        return urlpath
+
+    except Execption:
+        print(Excption)
+        
+
+def optimized():
+    # return an array of the image urlpaths in diffent sizes 
+    thumb = 30, 30
+    small = 320, 320
+    medium = 768, 768
+    large = 990, 990
+    xlarge = 1080, 1080
+
+    try:
+        thumbnail = optimizer(image, thumb, 'thumbnail')
+        small_image = optimizer(image, small, '320px')
+        medium_image = optimizer(image, medium, '640px')
+        large_image = optimizer(image, large, '768px')
+        xlarge_image = optimizer(image, xlarge, '1080px')
+        return thumbnail, small_image, medium_image, large_image, xlarge_image
+    except Exception as e:
+        print(e)
+
+
+def save(file):
+    # saves file after verifying its an image file
+    if image_extension(file.filename):
+        return optimized(file)
+    return {"message": "not image"}    
